db319a340a0cb087b9e518fbdae5afac7c86fcaa
Nicholas <Nicholas@DESKTOP-1P9BOF4>
2017-12-04 20:52:46 -0400

    working away

---
+++ src/test/java/ca/unbsj/cs2383/BloomFilterTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.unbsj.cs2383;
+import static org.junit.Assert.*;
+import  org.junit.*;
+import java.util.*;
+/**
+ *
+ * @author Nicholas
+ */
+public class BloomFilterTest {
+      @Test
+    public void ContainsTest()
+    {
+	BloomFilter BF = new BloomFilter();
+        BF.add("Smith Jim Test");
+	assertEquals(true, BF.contains("Smith Jim Test"));
+    }
+    
+
+}
